c9679195bb36ad2d53e7c721bb073cbf631adf0e
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2024-12-20 17:56:50 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,11 @@
+print("Program to check if you are eligible to drive the vehicle or not")
+# a = int(input"(Enter your age: ")
+# a = input("Enter your age:  "  )
+a = int(input("Enter your age: "))
+print("Your age is: " , a)
+
+if a >= 18:
+    print("ELIGIBLE")
+
+else:
+    print("NOT ELIGIBLE")
