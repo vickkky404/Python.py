dcd20a5f06f3e5e78fa4be9278e67fae2865a19f
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-06 19:51:30 +0530

    Add files via upload

---
+++ Array2.py
@@ -0,0 +1,7 @@
+# this is list array that can store element of diffrent datatypes..
+arr2 = [ 2 , "hello" , 2.5]
+print(arr2)
+
+# removes the perticular element entered inside the arr2.remove(INSERT THE ELEMENT TO BE REMOVED)
+arr2.remove(2)
+print(arr2)
