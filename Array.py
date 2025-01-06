58dd73307f4e383c5e813d35996a7199abcfc87b
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-06 19:51:11 +0530

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,11 @@
+# declaration of an array
+array = [2 ,4, 5 , 9]
+
+# printing a specific element using the indices in an array
+print(array[2])
+
+# pop operation in an array to remove a specific element in an array.
+array.pop(2)
+print(array)
+
+
