5df1c82b57b6d444b422986f66fa9f4579029761
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-07 23:36:27 +0530

    Add files via upload

---
+++ Input5.py
@@ -0,0 +1,4 @@
+# Write a Python program that converts temperature from Celsius to Fahrenheit.
+celsius = float(input("Enter temperature in Ceksius: "))
+fahrenhite = (celsius * 9/5) + 32
+print(f"The tempreatuare in farhennite is: {fahrenhite}")
