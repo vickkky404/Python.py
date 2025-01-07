bfdb7d8d84b66e12a3b2ecb7ec2c192dad9080cc
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-07 23:36:03 +0530

    Add files via upload

---
+++ Input4.py
@@ -0,0 +1,6 @@
+# Write a Python program to calculate the area of a circle using the radius provided by the user.
+
+import math
+radius =float(input("Enter the radius of the circle: "))
+area = math.pi * radius ** 2
+print(f"The radius of the circle is: {area}")
