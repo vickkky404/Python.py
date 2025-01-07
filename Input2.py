0c8f38563b4ccf9873bddb04f98fedf7d18ae2a7
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-01-07 23:35:43 +0530

    Add files via upload

---
+++ Input2.py
@@ -0,0 +1,6 @@
+# Write a Python program that takes two numbers as input and prints their sum.
+print("This Program prints sum of two number")
+num1 = input("Enter the first number:  ")
+num2 = input("Enter the second number: ")
+sum = num1 + num2
+print("The sum of two number is: ", sum)
