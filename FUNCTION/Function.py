f00ff5cb9d952ab6a3cf451f087e5af9b862b65a
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-02-04 00:46:15 +0530

    Add files via upload

---
+++ FUNCTION/Function.py
@@ -0,0 +1,15 @@
+# python function is a block of statement thar return the specific task.
+# function increases the code readablity and code reusablity
+
+# def function_name(parameters):
+        #statement
+#   return expression..
+
+
+# there are diffrent type of function present in python
+# built in lib function , User-defined function.
+
+def fun():
+    print("Hello World")\
+
+fun()
---
+++ FUNCTION/Function2.py
@@ -0,0 +1,11 @@
+def add(num1: int, num2: int) -> int:
+    """Add two number"""
+    num3 = num1 + num2
+
+
+    return num3
+
+num1 , num2 = 5 ,15
+ans = add(num1 , num2)
+
+print(f"addition of {num1} and {num2} results {ans}..." )
