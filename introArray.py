644c14c5637ef4eaa83ce27cac88a34622880c13
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-02-18 14:13:52 +0530

    Create introArray.py

---
+++ introArray.py
@@ -0,0 +1,19 @@
+# array is an object that provides a mechanism to store data..
+# array is benificial if you store data of same datatype..
+
+# in py array's size is dynaamic
+# ex of arr = group of student , group of employes , gropus of dogs.
+# ex = group of integer
+
+# imp
+# array and list are not same..
+# array use less memory than list
+
+# why do we need array??
+# to store data of same type
+
+# types of arr
+# one dimensional array= [101,102,103,104]
+# multi d array = [[100,200,300] [201,203]]
+
+# IMP = python doesnot support multidimensional array , we can still use it by use it using numpy..
