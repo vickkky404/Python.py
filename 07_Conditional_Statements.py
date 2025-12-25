d1e072933de6ee13069f8a72a6d0c68242346a0d
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-12-25 18:53:36 +0530

    Add 07_Conditional_Statements.py - If elif else

---
+++ 07_Conditional_Statements.py
@@ -0,0 +1,14 @@
+# If, elif, else statements
+num = 10
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+age = 20
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
